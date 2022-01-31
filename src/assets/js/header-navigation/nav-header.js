0c16d232249da6299c487380d09b65e1d60136e1
Ivan Volosevich <ivan.volosevich@vrpconsulting.com>
2022-01-31 19:44:34 +0300

    Added responsive design to Header and add Footer with full-responsive design

---
+++ src/assets/js/header-navigation/nav-header.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navShadow = document.getElementById('nav-shadow');
+  const navMenuToggle = document.getElementById('toggle-nav');
+  const navCloseMenuToggle = document.getElementById('close-nav-toggle');
+
+  navMenuToggle.onclick = function(){
+    navMenuToggle.checked === true ? navShadow.style.display = 'block' : navShadow.style.display = 'none'
+  };
+
+  navCloseMenuToggle.onclick = () => {
+    navMenuToggle.checked = false;
+    navShadow.style.display = 'none';
+  }
+
+  navShadow.onclick = () => {
+    navMenuToggle.checked = false;
+    navShadow.style.display = 'none';
+  }
+
+  console.log('navShadow: ', navShadow)
+  console.log('navMenuToggle: ', navMenuToggle.checked)
+})
