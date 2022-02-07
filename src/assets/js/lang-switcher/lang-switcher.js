c412e6373011e2ccf89536ed58c7ae7436f36527
ivanvolosevich <ivan.a.volosevich@mail.ru>
2022-02-07 21:06:37 +0300

    Updated localStorage lang saver

--- src/assets/js/lang-switcher/lang-switcher.js
+++ src/assets/js/lang-switcher/lang-switcher.js
@@ -1,24 +1,29 @@
-// window.addEventListener('load', () => {
-//   if (localStorage.getItem("langClient") === null || localStorage.getItem("langClient") === 'en') {
-//     if (document.location.pathname.slice(0, 3) === '/en') {
-//       document.location.href = document.location.pathname;
-//     }
-//   }
-// })
-
 document.addEventListener("DOMContentLoaded", () => {
-  let lang = document.all[0].lang;
-  localStorage.setItem('langClient', lang);
+  if (localStorage.getItem("langClient") === null || localStorage.getItem("langClient") === 'en') {
+    if (document.location.pathname.slice(0, 3) !== '/en') {
+      document.location.pathname = '/en' + document.location.pathname;
+    } else {
+      localStorage.setItem('langClient', document.all[0].lang);
+    }
+  } else if (localStorage.getItem("langClient") === 'ru') {
+    if (document.location.pathname.slice(0, 3) === '/en') {
+      document.location.pathname = document.location.pathname.slice(3, document.location.pathname.length);
+    } else {
+      localStorage.setItem('langClient', document.all[0].lang);
+    }
+  }
   
   const enLanguageStyles = document.querySelectorAll('.lang-switcher__en');
   const ruLanguageStyles = document.querySelectorAll('.lang-switcher__ru');
 
-  goToEnPage = function () {
-    document.location.href = '/en' + document.location.pathname;
+  goToEnPage = function() {
+    localStorage.setItem('langClient', 'en');
+    return document.location.href = '/en' + document.location.pathname;
   }
 
-  goToRuPage = function () {
-    document.location.href = document.location.pathname.replace(/^.{3}/, '');
+  goToRuPage = function() {
+    localStorage.setItem('langClient', 'ru');
+    return document.location.href = document.location.pathname.replace(/^.{3}/, '');
   }
 
   if (document.location.pathname.slice(0, 3) === '/en') {
